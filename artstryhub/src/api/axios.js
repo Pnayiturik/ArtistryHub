20c4dedda62ee4203e10c669f40815851e12a7e0
Pnayiturik <144005060+Pnayiturik@users.noreply.github.com>
2024-11-26 16:00:35 +0200

    Update axios.js

--- artstryhub/src/api/axios.js
+++ artstryhub/src/api/axios.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'http://localhost:8000'
+  baseURL: 'https://atristryhub.pythonanywhere.com'
 });
 
 // List of endpoints that don't require authentication
